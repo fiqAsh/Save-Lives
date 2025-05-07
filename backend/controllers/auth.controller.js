a358540fa36eb1f8eab3d8f8e8a9f9b1cc5a08a5
fiqAsh <ashfiq625@gmail.com>
2025-05-08 01:42:50 +0600

    j

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -169,63 +169,6 @@ export const getAllUser = async (req, res) => {
       .json({ message: "Failed to get users", error: error.message });
   }
 };
-//hjb
-// export const updateUser = async (req, res) => {
-//   try {
-//     const userId = req.user.id;
-//     const {
-//       name,
-//       email,
-//       password,
-//       bloodGroup,
-//       mobile,
-//       gender,
-//       age,
-//       weight,
-//       height,
-//       latitude,
-//       longitude,
-//     } = req.body;
-
-//     let user = await User.findById(userId);
-
-//     if (!user) {
-//       return res.status(404).json({ message: "User not found" });
-//     }
-
-//     let hashedPassword = user.password;
-
-//     if (password) {
-//       const salt = await bcrypt.genSalt(10);
-//       hashedPassword = await bcrypt.hash(password, salt);
-//     }
-
-//     user.name = name || user.name;
-//     user.email = email || user.email;
-//     user.password = hashedPassword;
-//     user.bloodGroup = bloodGroup || user.bloodGroup;
-//     user.mobile = mobile || user.mobile;
-//     user.gender = gender || user.gender;
-//     user.age = age || user.age;
-//     user.weight = weight || user.weight;
-//     user.height = height || user.height;
-//     if (latitude !== undefined && longitude !== undefined) {
-//       user.location = {
-//         type: "Point",
-//         coordinates: [longitude, latitude],
-//       };
-//     }
-//     console.log(user);
-
-//     await user.save();
-
-//     res.json({ message: "Profile updated successfully", user });
-//   } catch (error) {
-//     res
-//       .status(500)
-//       .json({ message: "Failed to update user", error: error.message });
-//   }
-// };
 
 export const updateUser = async (req, res) => {
   try {
